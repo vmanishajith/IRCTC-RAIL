475fb0b574361604ba4ce0bd7748cbd16c9702e5
Manish Ajith <manishleo157@gmail.com>
2023-04-06 21:24:49 +0530

    Password encryption using SHA-256 algorithm

--- src/main/java/com/irctc/rail/connect/service/IrctcService.java
+++ src/main/java/com/irctc/rail/connect/service/IrctcService.java
@@ -1,8 +1,11 @@
 package com.irctc.rail.connect.service;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.hash.Hashing;
 import com.irctc.rail.connect.User;
 import com.irctc.rail.connect.repository.IrctcRepository;
 
@@ -13,6 +16,11 @@ public class IrctcService {
 	IrctcRepository irctcRepository;
 	
 	public String saveUserLogin(User saveUser) {
+		String sha256hex = Hashing.sha256()
+				  .hashString(saveUser.getPassword(), StandardCharsets.UTF_8)
+				  .toString();
+		saveUser.setPassword(sha256hex);
+		System.out.println("pass --> "+sha256hex);
 		 irctcRepository.save(saveUser);
 			return "Login information of "+ saveUser.getName() + " is saved.";
 
